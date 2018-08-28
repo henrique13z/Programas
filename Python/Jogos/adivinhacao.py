8b3e103d05e7f36f48fbcb7e71c4d5885c8385ee
henrique13z <henrique13zzz@gmail.com>
2018-08-27 23:04:39 -0300

    27/08 - 23h04

--- Python/Jogos/adivinhacao.py
+++ Python/Jogos/adivinhacao.py
@@ -5,8 +5,9 @@ print("*************************************")
 numero_secreto = 42
 total_de_tentativas = 3
 
-while (total_de_tentativas > 0):
-    print("Tentativas: ", total_de_tentativas)
+#while (contador <= total_de_tentativas):
+for contador in range(1, total_de_tentativas +):
+    print("Tentativa  {} de {}".format(contador, total_de_tentativas))
     guess_str = input("Digite o seu numero de 0 a 100: ")
     guess = int(guess_str)
     print("Você digitou :", guess_str)
@@ -17,16 +18,10 @@ while (total_de_tentativas > 0):
 
     if (acertou):
         print("Você acertou!")
-        total_de_tentativas = 0
         print("Fim do jogo!")
-        input()
+        break
     elif(maior):
         print("Você chutou acima!")
-        total_de_tentativas = total_de_tentativas - 1
-        input()
     elif(menor):
         print("Você chutou abaixo!")
-        total_de_tentativas = total_de_tentativas - 1
-        input()
     #os.system('cls')
-print("Fim do jogo!")
